e1434635340a25392aa636c4464efb5ad2b6d6bb
Daniela Bravo <daniela.silvana.bravo@gmail.com>
2024-08-02 11:17:29 -0300

    listorti

--- src/paginas/Yapa.js
+++ src/paginas/Yapa.js
@@ -2,16 +2,19 @@ import '../estilos/yapa.css'
 
 const Yapa = (props) => {
 
-    return(
+    return (
         <div className='yapa'>
-            <h1>Rick and Morty</h1>
-           <iframe width="960" height="615" src="https://www.youtube.com/embed/1KnmKOvlEIw?si=8PZxpusyFUSwve7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+            <h1>It's Rick and Morty guys!</h1>
+
+            {/* <iframe width="100" height="140" src="https://www.myinstants.com/instant/rick-and-morty-mr-meeseeks/embed/" frameborder="0" scrolling="no"></iframe> */}
+            {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/1KnmKOvlEIw?si=8PZxpusyFUSwve7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>' */}
 
             <br/>
             <br/>
+            <br/>
+            <br/>
 
-
-           <iframe width="960" height="615" src="https://www.youtube.com/embed/YPrtsmluEO8?si=PicdbreVp1E_wNXp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/YPrtsmluEO8?si=PicdbreVp1E_wNXp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     )
 
