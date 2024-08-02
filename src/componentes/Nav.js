b527caea30884b89a1cdc4047f3e43838876491b
Daniela Bravo <daniela.silvana.bravo@gmail.com>
2024-08-02 12:06:03 -0300

    listortilla

--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/ejemplo1">Ejemplo 1</Link></li>
-                <li><Link to="/ejemplo2">Ejemplo 2 lcdtm</Link></li>
+                <li><Link to="/ejemplo2">Ejemplo 2</Link></li>
                 <li><Link to="/ejemplo3">Ejemplo 3</Link></li>
                 <li><Link to="/ejemplo4">Ejemplo 4</Link></li>
                 <li><Link to="/ejemplo5">Ejemplo 5</Link></li>
